1e599a5082fbbc3496f6c090280ac7381612e6b4
prasannajeet2002 <prasannajeetpradhan20@outlook.com>
2022-06-16 11:31:05 +0530

    Updates

---
+++ precache-manifest.ff8d3c7f75a71e9af7c55e90603b4f92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c02430cb9132bc71b747",
+    "url": "/expense-tracker/static/css/main.047e1ed2.chunk.css"
+  },
+  {
+    "revision": "c02430cb9132bc71b747",
+    "url": "/expense-tracker/static/js/main.c02430cb.chunk.js"
+  },
+  {
+    "revision": "aaf33d6a2fa40dccd0f7",
+    "url": "/expense-tracker/static/js/1.aaf33d6a.chunk.js"
+  },
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "c8d66180800a2994185a8cd901cb61a4",
+    "url": "/expense-tracker/index.html"
+  }
+];
